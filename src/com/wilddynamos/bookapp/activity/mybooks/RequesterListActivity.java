f663ae25b9b7590e567126abb6657b2ffb2a1e70
ZheQian <quizip@gmail.com>
2013-11-25 15:16:17 -0800

    change the requester image id in mybooks_requestlist_row.xml

--- src/com/wilddynamos/bookapp/activity/mybooks/RequesterListActivity.java
+++ src/com/wilddynamos/bookapp/activity/mybooks/RequesterListActivity.java
@@ -2,20 +2,16 @@ package com.wilddynamos.bookapp.activity.mybooks;
 
 import java.util.ArrayList;
 import java.util.HashMap;
- 
-import android.annotation.SuppressLint;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ListView;
+
 import com.wilddynamos.bookapp.R;
-import com.wilddynamos.bookapp.activity.post.PostListActivity;
  
 public class RequesterListActivity extends Activity {
     // All static variables
@@ -23,29 +19,11 @@ public class RequesterListActivity extends Activity {
     static final String KEY_REQUEST_NAME = "requester";
     static final String KEY_REQUESTER_IMAGE = "requester_image";
  
-    ListView list;
-    LazyAdapter adapter;
+    private ListView list;
+    private LazyAdapter adapter;
+    private Button declineAll;
     
-    @SuppressLint("HandlerLeak")
-	private Handler handler = new Handler() {
-
-		@Override
-    	public void handleMessage(Message msg){
-    		if(msg.what == -1)
-    			Toast.makeText(RequesterListActivity.this, "Oops!", Toast.LENGTH_SHORT).show();
-    		else if(msg.what == 1) {
-    			if(currentPage == 1)
-    				pour();
-    			else {
-    				loadData();
-    				pla.notifyDataSetChanged();
-    				loadProgress.setVisibility(ProgressBar.INVISIBLE);
-    			}
-    		} else
-    			Toast.makeText(RequesterListActivity.this, "What happened?", Toast.LENGTH_SHORT).show();
-    	}
-	};
-	
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
