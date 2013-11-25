60edbffe891a14c4e577e0a875915a16cada6016
ZheQian <quizip@gmail.com>
2013-11-25 14:39:45 -0800

    start requesterlist

--- src/com/wilddynamos/bookapp/activity/mybooks/RequesterListActivity.java
+++ src/com/wilddynamos/bookapp/activity/mybooks/RequesterListActivity.java
@@ -3,13 +3,19 @@ package com.wilddynamos.bookapp.activity.mybooks;
 import java.util.ArrayList;
 import java.util.HashMap;
  
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import com.wilddynamos.bookapp.R;
+import com.wilddynamos.bookapp.activity.post.PostListActivity;
  
 public class RequesterListActivity extends Activity {
     // All static variables
@@ -19,7 +25,27 @@ public class RequesterListActivity extends Activity {
  
     ListView list;
     LazyAdapter adapter;
- 
+    
+    @SuppressLint("HandlerLeak")
+	private Handler handler = new Handler() {
+
+		@Override
+    	public void handleMessage(Message msg){
+    		if(msg.what == -1)
+    			Toast.makeText(RequesterListActivity.this, "Oops!", Toast.LENGTH_SHORT).show();
+    		else if(msg.what == 1) {
+    			if(currentPage == 1)
+    				pour();
+    			else {
+    				loadData();
+    				pla.notifyDataSetChanged();
+    				loadProgress.setVisibility(ProgressBar.INVISIBLE);
+    			}
+    		} else
+    			Toast.makeText(RequesterListActivity.this, "What happened?", Toast.LENGTH_SHORT).show();
+    	}
+	};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
