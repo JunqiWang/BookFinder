ba2c4f89944fe0ff21ea087a42a11f2544b8cc06
JunqiWang <junqiw@andrew.cmu.edu>
2013-11-27 21:20:49 -0800

    d

--- src/com/wilddynamos/bookapp/ws/remote/action/WaitingForMessage.java
+++ src/com/wilddynamos/bookapp/ws/remote/action/WaitingForMessage.java
@@ -13,15 +13,15 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.wilddynamos.bookapp.R;
-import com.wilddynamos.bookapp.activity.MultiWindowActivity;
 import com.wilddynamos.bookapp.activity.mybooks.MyPostDetailActivity;
+import com.wilddynamos.bookapp.activity.mybooks.MyRequestDetailActivity;
 import com.wilddynamos.bookapp.ws.remote.Connection;
 
-public class WaitingForRequest extends Thread {
+public class WaitingForMessage extends Thread {
 	
 	private Service service;
 	
-	public WaitingForRequest(Service service) {
+	public WaitingForMessage(Service service) {
 		this.service = service;
 	}
 	
@@ -55,6 +55,24 @@ public class WaitingForRequest extends Thread {
 							notiIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 					
 					noti.setLatestEventInfo(service, "Book Finder", "New Request Coming", 
+				                pendingIntent);
+					
+					notiMngr.notify(0, noti);
+				} else 	if(str.contains("Res")) {
+					int bookId = Integer.parseInt(str.substring(3));
+					Notification noti = new Notification(R.drawable.app_cover,  
+				                "New Response Coming", System.currentTimeMillis());
+					
+					noti.flags = Notification.FLAG_AUTO_CANCEL;
+					
+					Intent notiIntent = new Intent(service, MyRequestDetailActivity.class);
+					notiIntent.putExtra("id", bookId);
+					notiIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK); 
+					
+					PendingIntent pendingIntent = PendingIntent.getActivity(service, 0,  
+							notiIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+					
+					noti.setLatestEventInfo(service, "Book Finder", "New Response Coming", 
 				                pendingIntent);
 					
 					notiMngr.notify(0, noti);
