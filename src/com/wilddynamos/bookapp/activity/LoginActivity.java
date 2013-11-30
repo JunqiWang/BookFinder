be4ec6cfa3fedfa9aaf55abcd63ecfa0d9f37187
chengzh1 <cheng.zhang@sv.cmu.edu>
2013-11-29 23:17:00 -0800

    refine

--- src/com/wilddynamos/bookapp/activity/LoginActivity.java
+++ src/com/wilddynamos/bookapp/activity/LoginActivity.java
@@ -14,7 +14,6 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.text.Editable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
